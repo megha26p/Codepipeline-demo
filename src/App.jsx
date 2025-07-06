7b0d3dbebdfd10c9246e988ca510fa89056a9687
Megha Patro <ece.23becc98@silicon.ac.in>
2025-07-07 02:26:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>This code is from the devops class</h1>
+        <p>This code is from the devops class</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
